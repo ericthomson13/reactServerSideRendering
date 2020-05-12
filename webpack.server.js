390a1e1b8b827507ade6dae2b5031dae6ff46560
et <eric.thomson13@gmail.com>
2020-05-12 15:21:00 -0600

    Webpack Setup

---
+++ webpack.server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+modules.exports = {
+  // Inform webpack this is nodeJS bundle not for browser
+  target: 'node',
+  // Root file of server application
+  entry: './server/src/index.js',
+  // Tell webpack where to put output file
+  output: {
+    fileName: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  // Tell webpack to run Babel on every file it runs thorough
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', {
+              targets: {
+                browsers: ['last 2 versions']
+              }
+            }],
+          ]
+        }
+      }
+    ]
+  }
+};
